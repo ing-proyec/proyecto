d6319e2f8eb9eb6d267e7fae5e323407e59460ec
Heiner <heinera123@gmail.com>
2021-07-01 11:44:17 -0600

    boton heiner

--- Infotanica/Form1.Designer.cs
+++ Infotanica/Form1.Designer.cs
@@ -31,15 +31,17 @@ namespace Infotanica
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.bienvenida = new System.Windows.Forms.ToolTip(this.components);
-            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -47,6 +49,20 @@ namespace Infotanica
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(189, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(436, 109);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Infótanica";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
@@ -66,19 +82,14 @@ namespace Infotanica
             // 
             this.bienvenida.ToolTipTitle = "Bienvenido al sistema";
             // 
-            // label2
+            // button1
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(189, 107);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(436, 109);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Infótanica";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.button1.Location = new System.Drawing.Point(280, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Heiner";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -103,6 +114,7 @@ namespace Infotanica
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ToolTip bienvenida;
+        private System.Windows.Forms.Button button1;
     }
 }
 
