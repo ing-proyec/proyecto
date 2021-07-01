937bc1f6d28f4bc8a704909400c75a4afe4b99ed
Pablo picado <pablo.picado1002@gmail.com>
2021-07-01 11:48:00 -0600

    Label pablo

--- Infotanica/Form1.Designer.cs
+++ Infotanica/Form1.Designer.cs
@@ -31,16 +31,18 @@ namespace Infotanica
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.bienvenida = new System.Windows.Forms.ToolTip(this.components);
-            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
@@ -49,6 +51,15 @@ namespace Infotanica
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Heiner";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -82,14 +93,15 @@ namespace Infotanica
             // 
             this.bienvenida.ToolTipTitle = "Bienvenido al sistema";
             // 
-            // button1
+            // label3
             // 
-            this.button1.Location = new System.Drawing.Point(280, 375);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 37);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Heiner";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Pablooooooo";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
@@ -115,6 +127,7 @@ namespace Infotanica
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ToolTip bienvenida;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Infotanica/Form1.cs
+++ Infotanica/Form1.cs
@@ -17,5 +17,9 @@ namespace Infotanica
             InitializeComponent();
         }
 
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
